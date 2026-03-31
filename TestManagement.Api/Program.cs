57dcf449144908a8ae78d2d0865cafbf30d758b0
Duong Tran <d.tran.1@campus.tu-berlin.de>
2026-03-31 19:58:23 +0200

    feat: add initial API setup with PostgreSQL configuration and Docker Compose

---
+++ TestManagement.Api/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using TestManagement.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+// to debug connection string
+// make sure the connection string in appsettings.Development.json is correct and the database server is running
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//Console.WriteLine($"CONNECTION STRING = {connectionString}");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+
+}
+
+app.MapGet("/", () => "Hello world!");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
