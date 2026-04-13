2426a0e9a0a26730c1d38c8831035f7975426d6e
Duong Tran <d.tran.1@campus.tu-berlin.de>
2026-04-13 18:03:10 +0200

    feat: initialize tests and src projects

---
+++ SimulationService/src/Simulation.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Simulation.Core;
+
+public class Class1
+{
+
+}
---
+++ SimulationService/tests/Simulation.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Simulation.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
