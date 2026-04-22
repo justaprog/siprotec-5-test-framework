7941c6182798294290197746feaab927cdba3cf9
Duong Tran <d.tran.1@campus.tu-berlin.de>
2026-04-22 21:03:19 +0200

    deploy: comment out HTTPS redirection in development environment

--- TestManagementService/src/TestManagement.Api/Program.cs
+++ TestManagementService/src/TestManagement.Api/Program.cs
@@ -50,7 +50,7 @@ if (app.Environment.IsDevelopment())
 
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 // enable the CORS middleware, should be placed before UseAuthorization and 
 // MapControllers to ensure CORS headers are included in responses
 // to preflight requests and API endpoints
